5876ef39cf63a525eb72a42efa9b102ee208ce39
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 15:19:06 +0100

    Added desktop nav and size store to Header

--- src/lib/Navigation/Header.svelte
+++ src/lib/Navigation/Header.svelte
@@ -1,32 +1,41 @@
 <script>
-    import {createEventDispatcher} from 'svelte'
-    const dispatch = createEventDispatcher();
+	import { createEventDispatcher, getContext } from 'svelte';
+	const dispatch = createEventDispatcher();
 
-    import Logo from './Logo.svelte'
-    import MobileNav from './MobileNav.svelte'
-import BurgerButton from './BurgerButton.svelte'
+	import Logo from './Logo.svelte';
+	import MobileNav from './MobileNav.svelte';
+	import TabDesNav from './TabDesNav.svelte';
+	import BurgerButton from './BurgerButton.svelte';
 
-    let showMobileNav = false
+	let showMobileNav = false,
+		size = getContext('size');
 
-    const toggleMobileNav = () => {
-        showMobileNav = !showMobileNav
-        dispatch('toggleMenu')
-    }
+	const toggleMobileNav = () => {
+		showMobileNav = !showMobileNav;
+		dispatch('toggleMenu');
+	};
 </script>
 
 <header>
-    <Logo/>
-    <BurgerButton on:toggleMenu={toggleMobileNav}/>
-    {#if showMobileNav}
-         <MobileNav/>
-    {/if}
+	<Logo />
+	{#if $size === 'mobile'}
+		<BurgerButton on:toggleMenu={toggleMobileNav} />
+		{#if showMobileNav}
+			<MobileNav />
+		{/if}
+    {:else}
+        <TabDesNav />
+	{/if}
 </header>
 
 <style lang="scss">
-    @import '../../scss-styles/mixins';
-    header {
-        @include flex(row nowrap, space-between, center);
-        padding: 2.8rem 2.4rem;
-        position: relative;
-    }
+	@import '../../scss-styles/mixins';
+	header {
+		@include flex(row nowrap, space-between, center);
+		padding: 2.8rem 2.4rem;
+		position: relative;
+		width: 100%;
+		max-width: 111rem;
+		margin: auto;
+	}
 </style>
