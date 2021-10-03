c53b65ee236321c559407f956de5b6a2a76f9fe5
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 16:30:22 +0100

    created a new global grid

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,13 @@
 	import { setContext, onMount } from 'svelte';
 	import Header from '$lib/Navigation/Header.svelte';
 	import Overlay from '$lib/Decorations/Overlay.svelte';
-	import {calcRealSize} from '$lib/scripts/helperFunctions.js';
+	import { calcRealSize } from '$lib/scripts/helperFunctions.js';
 
-	let windowWidth, scrollbarWidth, realWidth = writable(''), mounted = false, showOverlay = false;
+	let windowWidth,
+		scrollbarWidth,
+		realWidth = writable(''),
+		mounted = false,
+		showOverlay = false;
 	$: realWidth.set(calcRealSize(windowWidth, scrollbarWidth));
 	setContext('size', realWidth);
 
@@ -28,10 +32,32 @@
 <slot />
 
 <style lang="scss">
+	@import '../scss-styles/mixins';
 	:global(body) {
 		width: 100%;
 		height: 200vh;
 		background-image: url('/images/shared/bg-dark.png');
 		background-repeat: repeat;
+	}
+
+	:global(div#svelte) {
+		display: grid;
+		grid-template-columns: minmax(2.4rem, 4rem) 1fr minmax(2.4rem, 4rem);
+		grid-template-areas:
+			'. header .'
+			'. main .'
+			'. footer .';
+
+		@include mq(tablet) {
+			grid-template-columns: minmax(4rem, 1fr) minmax(70rem, 90rem) minmax(4rem, 1fr);
+		}
+
+		@include mq(desktop) {
+			grid-template-columns: 1fr minmax(0rem, 10.5rem) 90rem minmax(0rem, 10.5rem) 1fr;
+			grid-template-areas:
+				'. header header header .'
+				'. . main . .'
+				'footer footer footer footer footer';
+		}
 	}
 </style>
