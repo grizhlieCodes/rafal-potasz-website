521ed14fc23eb80d68aa83341918da302c253dbe
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 15:21:45 +0100

    created Hero component

---
+++ src/lib/Home/hero/Hero.svelte
@@ -0,0 +1,32 @@
+<script>
+    import HeroImage from './HeroImage.svelte'
+    import HeroText from './HeroText.svelte'
+	import Section from '$lib/Core/Section.svelte'
+	import {getContext} from 'svelte';
+
+	let size = getContext('size')
+
+	$: console.log($size)
+</script>
+
+<Section>
+	<div class="flex-container">
+		<HeroImage />
+		<HeroText />
+	</div>
+</Section>
+
+<style lang="scss">
+	@import '../../../scss-styles/mixins';
+
+	.flex-container {
+		width: 100%;
+		@include flex(column nowrap, start, center);
+		gap: 6rem;
+
+		
+        @include mq(tablet){
+            @include eflex(row-reverse nowrap, space-between, center);
+        }
+	}
+</style>
