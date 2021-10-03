ec2893b0ddb723bd1e7cfbd313864621f71aa01c
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 15:20:49 +0100

    added hero-img animation to animations

--- src/lib/scripts/animations.js
+++ src/lib/scripts/animations.js
@@ -37,3 +37,94 @@ export const flyMultipleItems = (node, direction, itemNode) => {
         })
     })
 }
+
+export const moveOnScroll = (node, distance) => {
+    const nodeClass = node.classList[0]
+    const value = distance
+    const top = `-${value}rem`
+    const bottom = `${value}rem`
+    const left = `-${value}rem`
+    const right = `${value}rem`
+    if (nodeClass === 'rafal') {
+        return
+    }
+    if (nodeClass === 'computer') {
+        gsap.to(node, {
+            scrollTrigger: {
+                trigger: node,
+                start: "top 50rem",
+                end: "bottom 0%",
+                scrub: 1,
+                // markers: 1
+            },
+            y: bottom,
+        })
+        return
+    }
+    if (nodeClass === 'eagle') {
+        gsap.to(node, {
+            scrollTrigger: {
+                trigger: node,
+                start: "top 50rem",
+                end: "bottom 0%",
+                scrub: 1
+            },
+            // opacity: 1,
+            x: right,
+            y: bottom,
+        })
+        return
+    }
+    if (nodeClass === 'book') {
+        gsap.to(node, {
+            scrollTrigger: {
+                trigger: node,
+                start: "top 50rem",
+                end: "bottom 0%",
+                scrub: 1
+            },
+            x: left,
+            y: bottom,
+        })
+        return
+    }
+    if (nodeClass === 'watch') {
+        gsap.to(node, {
+            scrollTrigger: {
+                trigger: node,
+                start: "top 50rem",
+                end: "bottom 0%",
+                scrub: 1
+            },
+            x: left,
+        })
+        return
+    }
+    if (nodeClass === 'rust') {
+        gsap.to(node, {
+            scrollTrigger: {
+                trigger: node,
+                start: "top 50rem",
+                end: "bottom 0%",
+                scrub: 1
+            },
+            x: left,
+            y: top,
+        })
+        return
+    }
+    if (nodeClass === 'statue') {
+        gsap.to(node, {
+            scrollTrigger: {
+                trigger: node,
+                start: "top 50rem",
+                end: "bottom 0%",
+                scrub: 1
+            },
+            // opacity: 1,
+            x: right,
+            y: top,
+        })
+        return
+    }
+}
