bebe034bc121a59b48119fa9c7d12af242e5e5dd
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 15:22:23 +0100

    created TabDesNav component

---
+++ src/lib/Navigation/TabDesNav.svelte
@@ -0,0 +1,62 @@
+<script>
+    	import { flyItem } from '$lib/scripts/animations.js';
+	import { page } from '$app/stores';
+	$: path = $page.path;
+	import { capitaliseFirstLetter } from '$lib/scripts/helperFunctions.js';
+	import Icon from '$lib/Decorations/Icon.svelte';
+	let navData = [
+		{ name: 'home', link: '/' },
+		{ name: 'about', link: '/about' },
+		{ name: 'portfolio', link: '/portfolio' }
+	];
+</script>
+
+<nav class="navigation">
+	{#each navData as { name, link }, i}
+		<a
+			href={link}
+			class="navigation__link"
+			class:active={path === link}
+			use:flyItem={['-20rem', 0.15, i]}>
+			<Icon {name} width="1.8rem" />
+
+			<p class="navigation__link-text">{capitaliseFirstLetter(name)}</p>
+		</a>
+	{/each}
+</nav>
+
+<style lang="scss">
+	@import '../../scss-styles/mixins';
+
+	.navigation {
+		z-index: v(z-index-priority);
+		@include flex(row nowrap, start, start);
+		gap: 3rem;
+	}
+
+	a.navigation__link {
+		transition: color 250ms;
+		font-size: 1.6rem;
+		font-family: v(fira);
+		color: v(clr-highlight-3);
+		z-index: 5;
+		@include flex(row nowrap, start, center);
+		gap: 2.2rem;
+
+		&.active {
+			color: v(clr-accent-cyan);
+		}
+
+		&:hover:not(.active) {
+			color: v(clr-highlight-2);
+		}
+	}
+
+	:global(a.navigation__link.active svg path) {
+		fill: v(clr-accent-cyan) !important;
+	}
+
+	:global(a.navigation__link:hover:not(.active) svg path) {
+		fill: v(clr-highlight-2) !important;
+	}
+</style>
