f78df0884a020486b77f9cc7850f6c187db65fe3
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 15:18:15 +0100

    Added header and size store to __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,30 @@
 <script>
+	import { writable } from 'svelte/store';
+	import { setContext, onMount } from 'svelte';
 	import Header from '$lib/Navigation/Header.svelte';
-	import Overlay from '$lib/Decorations/Overlay.svelte'
+	import Overlay from '$lib/Decorations/Overlay.svelte';
+	import {calcRealSize} from '$lib/scripts/helperFunctions.js';
 
-	let showOverlay = false
-	$: console.log(showOverlay)
+	let windowWidth, scrollbarWidth, realWidth = writable(''), mounted = false, showOverlay = false;
+	$: realWidth.set(calcRealSize(windowWidth, scrollbarWidth));
+	setContext('size', realWidth);
+
+	onMount(() => {
+		mounted = true;
+		if (mounted) {
+			scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+		}
+	});
 </script>
+
+<svelte:window bind:innerWidth={windowWidth} />
+
 {#if showOverlay}
-	 <Overlay />
+	<Overlay />
 {/if}
 
-<Header on:toggleMenu={() => showOverlay = !showOverlay} />
-	
+<Header on:toggleMenu={() => (showOverlay = !showOverlay)} />
+
 <slot />
 
 <style lang="scss">
