26f16f6b500f11d5f245159dc97f5dbec0dd9819
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 15:20:32 +0100

    Added custom mq function to helpers

--- src/lib/scripts/helperFunctions.js
+++ src/lib/scripts/helperFunctions.js
@@ -3,3 +3,17 @@ export const capitaliseFirstLetter = (string) => {
     array[0] = array[0].toUpperCase()
     return array.join('')
 }
+
+
+export const calcRealSize = (winWidth, scrollWidth) => {
+    let width = winWidth - scrollWidth;
+    let result
+    if (width >= 1024) {
+        result = 'desktop';
+    } else if (width >= 768) {
+        result = 'tablet';
+    } else {
+        result = 'mobile';
+    }
+    return result
+};
