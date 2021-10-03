8f4abcdd558507b84af2962553b6253c28cbd20c
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-03 15:21:33 +0100

    created section component

---
+++ src/lib/Core/Section.svelte
@@ -0,0 +1,43 @@
+<script>
+    
+</script>
+
+<section class="section--900">
+
+    <slot />
+
+</section>
+
+<style lang="scss">
+    @import '../../scss-styles/mixins';
+
+
+
+    section {
+		--padding: 2.4rem;
+		width: 100%;
+		height: auto;
+		display: grid;
+		justify-items: center;
+		align-items: start;
+		padding: v(padding);
+		overflow: hidden;
+
+		@include mq(tablet){
+			--padding: 4rem;
+		}
+	}
+
+	section.section--900 {
+		grid-template-columns: 1fr minmax(27rem, 90rem) 1fr;
+	}
+
+	:global(section.section--900 > *) {
+		grid-column: 2/3;
+	}
+
+	section:nth-of-type(1){
+		padding-top: 5rem;
+	}
+    
+</style>
