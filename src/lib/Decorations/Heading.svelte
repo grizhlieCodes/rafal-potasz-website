a2639b1f5432fbe1f677819830b584a0caa97a91
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-04 16:37:26 +0100

    Created heading component

---
+++ src/lib/Decorations/Heading.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let type = h2,
+		content = '',
+		content2 = '',
+		headingClass = '';
+</script>
+
+{#if type === '1'}
+	<h1 class={headingClass}>
+		{content}
+		{#if content2 !== ''}
+			<br />
+			{content2}
+		{/if}
+	</h1>
+{:else if type === '2'}
+	<h2 class={headingClass}>{content}</h2>
+{/if}
+
+<style lang="scss">
+	@import '../../scss-styles/mixins';
+
+	:is(h1, h2) {
+		font-family: v(fira);
+		font-weight: 700;
+		color: v(clr-text-heading);
+		transition: color 400ms;
+	}
+
+	h1 {
+		font-size: fluid(desktop, 3.5, 5.5);
+		line-height: fluid(desktop, 3.5, 5.5);
+	}
+
+    h2 {
+        font-size: fluid(desktop, 2.5, 4);
+        line-height: fluid(desktop, 2.5, 4);
+        text-transform: uppercase;
+    }
+</style>
