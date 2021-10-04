4b224eb9db7b9f97b2246fac992339655391f87e
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-04 16:36:00 +0100

    Added netlify bits

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
 import preprocess from 'svelte-preprocess';
+import netlify from '@sveltejs/adapter-netlify'
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: netlify()
 	},
 	preprocess: preprocess()
 };
