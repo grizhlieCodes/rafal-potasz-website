c06eff6da5dd1cbb7b5549e79ddc37c11d533829
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-04 16:37:39 +0100

    created lines component

---
+++ src/lib/Decorations/Line.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let direction, dimension, top = '0', left = '0', bottom = '0', right = '0';
+</script>
+
+{#if direction === 'horizontal'}
+	<span class="line horizontal" style="width: {dimension}; top: {top};bottom: {bottom};left: {left};right: {right};" />
+{:else}
+	<span class="line vertical" style="height: {dimension}; top: {top};bottom: {bottom};left: {left};right: {right};" />
+{/if}
+
+<style lang="scss">
+	@import '../../scss-styles/mixins';
+
+	span {
+		display: block;
+		position: absolute;
+		background: v(clr-line-bg);
+	}
+	span.horizontal {
+		height: 1px;
+	}
+
+    span.vertical {
+        width: 1px;
+    }
+</style>
---
+++ src/lib/Decorations/Lines.svelte
@@ -0,0 +1,75 @@
+<script>
+	import { onMount } from 'svelte';
+	import Line from './Line.svelte';
+	let mounted = false,
+		headerHeight = 0,
+		heroHeight = 0,
+		philHeight,
+		allComplete = false,
+		loaded;
+
+	onMount(() => {
+		mounted = true;
+		window.addEventListener('load', () => (loaded = true));
+	});
+
+	$: if (mounted && loaded) updateHeights();
+
+	const updateHeights = () => {
+		headerHeight = returnElHeight('header');
+		heroHeight = returnElHeight('section.hero');
+		setTimeout(() => {
+			allComplete = true;
+		}, 500);
+	};
+	const returnElHeight = (selector) => {
+		const el = document.querySelector(`${selector}`);
+		const elHeight = el.clientHeight;
+		return elHeight;
+	};
+</script>
+
+<!-- {#if allComplete} -->
+<div class="lines-container">
+	<div class="section first-section" style="height: {headerHeight}px;">
+		<Line direction="horizontal" dimension="100vw" top="20%" />
+		<Line direction="vertical" dimension="100vh" left="5%" />
+		<span class="circle" />
+	</div>
+	<div class="section first-section" style="height: {heroHeight}px;">
+		<!-- <Line direction="horizontal" dimension="100vw" top="20%" />
+		<Line direction="vertical" dimension="100vh" left="5%" />
+		<span class="circle" /> -->
+	</div>
+</div>
+<!-- {/if} -->
+
+<style lang="scss">
+	@import '../../scss-styles/mixins';
+
+	span.circle {
+		display: block;
+		position: absolute;
+		width: 90%;
+		max-width: 100rem;
+		aspect-ratio: 1;
+		background: transparent;
+		border-radius: 50%;
+		border: 1px solid v(clr-line-bg);
+		transform: translate(-50%, -50%);
+	}
+
+	.lines-container {
+		z-index: v(z-index-bg);
+		grid-column: 1 / -1;
+		grid-row: 1 / -1;
+		width: 100%;
+		height: 100%;
+		position: relative;
+
+		.section {
+			width: 100%;
+			position: relative;
+		}
+	}
+</style>
