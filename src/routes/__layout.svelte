91c420e5a742e508600a34dc4d57c1746ae476e9
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-04 16:38:05 +0100

    updated grid

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -28,25 +28,27 @@
 {/if}
 
 <Header on:toggleMenu={() => (showOverlay = !showOverlay)} />
-
 <slot />
 
 <style lang="scss">
 	@import '../scss-styles/mixins';
+
+
 	:global(body) {
 		width: 100%;
 		height: 200vh;
-		background-image: url('/images/shared/bg-light.png');
+		background-color: v(clr-body-bg);
+		// background-image: url('/images/shared/bg-light2.png');
 		background-repeat: repeat;
 	}
 
 	:global(body.dark) {
-		background-image: url('/images/shared/bg-dark.png');
+		// background-image: url('/images/shared/bg-dark.png');
 	}
 
 	:global(div#svelte) {
 		display: grid;
-		grid-template-columns: minmax(2.4rem, 4rem) 1fr minmax(2.4rem, 4rem);
+		grid-template-columns: minmax(2.4rem, 4rem) minmax(32.7rem, 1fr) minmax(2.4rem, 4rem);
 		grid-template-areas:
 			'. header .'
 			'. main .'
