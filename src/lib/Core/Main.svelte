869fbf9b69dc8be77a59ec573aec8a80b233379d
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-05 19:02:14 +0100

    created Main comp for better control

---
+++ src/lib/Core/Main.svelte
@@ -0,0 +1,17 @@
+<script>
+    
+</script>
+
+<main>
+
+</main>
+
+<style lang="scss">
+    main {
+        width: 100%;
+        grid-area: main;
+        display: grid;
+        grid-template-columns: 1fr;
+        
+    }
+</style>
