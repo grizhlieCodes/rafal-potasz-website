f03ff6ab9ad4caa3d388c62c9aadc6fd0698c0c7
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-08 16:22:43 +0100

    Updated About.svelte

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Main from '$lib/Core/Main.svelte'
+    import Heading from '$lib/Decorations/Heading.svelte'
+</script>
+
+<Main>
+    <Heading type="1" content="about" />
+</Main>
+
+<style lang="scss">
+    
+</style>
