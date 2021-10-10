9b3059cde12884eb456dbeccb1773aab6514e7ce
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-11 00:16:13 +0100

    Removed comments

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,30 @@
 <script>
 	import portfolio from '$lib/stores/portfolio.js';
-	import Logo from '$lib/Navigation/Logo.svelte'
-    import Hero from '$lib/Home/hero/Hero.svelte'
-	import Lines from '$lib/Decorations/Lines.svelte'
-	import Main from '$lib/Core/Main.svelte'
-import Philosophy from '$lib/Home/philosophy/Philosophy.svelte';
-import Technologies from '$lib/Home/technologies/Technologies.svelte';
-	import Portfolio from '$lib/Home/portfolio/Portfolio.svelte'
+	import Logo from '$lib/Navigation/Logo.svelte';
+	import Hero from '$lib/Home/hero/Hero.svelte';
+	import Lines from '$lib/Decorations/Lines.svelte';
+	import Main from '$lib/Core/Main.svelte';
+	import Philosophy from '$lib/Home/philosophy/Philosophy.svelte';
+	import Technologies from '$lib/Home/technologies/Technologies.svelte';
+	import Portfolio from '$lib/Home/portfolio/Portfolio.svelte';
 	import ContactForm from '$lib/Home/contact-form/ContactForm.svelte';
-	let recalculate
+	let recalculate;
 
 	const recalculateLines = () => {
-		recalculate.rerunLines()
+		recalculate.rerunLines();
 	};
-
 </script>
-
-<!-- {#each $portfolio as {vimeoEmbed, name}}
-	<div style="padding:56.25% 0 0 0;position:relative;">
-		<iframe
-			src="https://player.vimeo.com/video/{vimeoEmbed}&color=ffffff&title=0&byline=0&portrait=0&autoplay=1&loop=1&muted=1&autopause=0"
-			style="position:absolute;top:0;left:0;width:100%;height:100%;"
-			frameborder="0"
-			allow="autoplay" 
-            title="{name}"/>
-	</div>
-{/each} -->
 
 <Main>
 	<Hero />
 	<Philosophy />
 	<Technologies on:recalculateLines={recalculateLines} />
-	<Portfolio on:recalculateLines={recalculateLines}/>
+	<Portfolio on:recalculateLines={recalculateLines} />
 	<ContactForm />
 </Main>
 
-<Lines bind:this={recalculate}/>
+<Lines bind:this={recalculate} />
 
 <style lang="scss">
 	@import '../scss-styles/mixins';
-	// main {
-	// 	grid-area: main;
-	// 	@include flex(column nowrap, start, start);
-	// 	gap: 12rem;
-	// }
-
 </style>
