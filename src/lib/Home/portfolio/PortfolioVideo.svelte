2312127d3e178d0d6ff71c1780d112df2c81b63d
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-21 16:45:40 +0100

    PortfolioVideo: Rect -> laptop on desktop viewport

--- src/lib/Home/portfolio/PortfolioVideo.svelte
+++ src/lib/Home/portfolio/PortfolioVideo.svelte
@@ -1,34 +1,36 @@
 <script>
 	export let project;
 	import VideoOnly from './VideoOnly.svelte';
+	import { getContext } from 'svelte';
+
+	let size = getContext('size');
 
 	$: vimeoEmbed = project.vimeoEmbed;
 	$: name = project.name;
 </script>
 
-<!-- <div class="video-container" data-direction="right">
-    <div style="padding:56.25% 0 0 0;position:relative;">
-        <iframe
-            src="https://player.vimeo.com/video/{vimeoEmbed}&color=ffffff&title=0&byline=0&portrait=0&autoplay=1&loop=1&muted=1&autopause=0&background=1"
-            style="position:absolute;top:0;left:0;width:100%;height:100%;"
-            frameborder="0"
-            allow="autoplay"
-            title={name} 
-			loading="lazy"/>
-    </div>
-</div> -->
-
-<VideoOnly>
-
-	<iframe
-		src="https://player.vimeo.com/video/{vimeoEmbed}&color=ffffff&title=0&byline=0&portrait=0&autoplay=1&loop=1&muted=1&autopause=0&background=1"
-		
-		frameborder="0"
-		allow="autoplay"
-		title={name}
-		loading="lazy" />
-
-</VideoOnly>
+{#if $size !== 'desktop'}
+	<div class="video-container" data-direction="right">
+		<div style="padding:56.25% 0 0 0;position:relative;">
+			<iframe
+				src="https://player.vimeo.com/video/{vimeoEmbed}&color=ffffff&title=0&byline=0&portrait=0&autoplay=1&loop=1&muted=1&autopause=0&background=1"
+				style="position:absolute;top:0;left:0;width:100%;height:100%;"
+				frameborder="0"
+				allow="autoplay"
+				title={name}
+				loading="lazy" />
+		</div>
+	</div>
+{:else}
+	<VideoOnly>
+		<iframe
+			src="https://player.vimeo.com/video/{vimeoEmbed}&color=ffffff&title=0&byline=0&portrait=0&autoplay=1&loop=1&muted=1&autopause=0&background=1"
+			frameborder="0"
+			allow="autoplay"
+			title={name}
+			loading="lazy" />
+	</VideoOnly>
+{/if}
 <!-- 775, 443 -->
 
 <style lang="scss">
